821afb46d3b720366de9daa01f08933ddc17ee5b
luogao <luogao_lg@sina.com>
2018-10-30 16:16:40 +0800

    update

--- index.js
+++ index.js
@@ -1,59 +1,68 @@
-var app = require('express')();
-var express = require('express')
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var count = 0;
+const app = require('express')();
+const express = require('express')
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+let count = 0;
+
+// 设置静态文件夹
 app.use(express.static(__dirname))
 
-
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
+	console.log("\33[2J");
 	console.log('a user connected');
-	socket.on('disconnect', function() {
-		console.log('user disconnected');
+
+	// send 方法来给客户端发消息
+	socket.send('hello world')
+
+	socket.on('message', function (msg) {
+		console.log(msg)
+		socket.send('nice to meet you')
 	});
+
 });
 
-http.listen(4000, function() {
+server.listen(4000, function () {
 	console.log('listening on:4000');
 });
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req, res) {
+// 	res.sendFile(__dirname + '/index.html');
+// });
 
-app.get('/room1', function(req, res) {
-	res.sendFile(__dirname + '/room1.html');
-});
+// app.get('/room1', function(req, res) {
+// 	res.sendFile(__dirname + '/room1.html');
+// });
 
-io.emit('some event', {
-	for: 'everyone'
-});
+// io.emit('some event', {
+// 	for: 'everyone'
+// });
 
-io.on('connection', function(socket) {
-	socket.broadcast.emit('hi');
-});
+// io.on('connection', function(socket) {
+// 	socket.broadcast.emit('hi');
+// });
 
-io.on('connection', function(socket) {
-	count++;
-	console.log('User connected' + count + 'user(s) present');
-	socket.emit('chat message', '你上线了')
-	socket.emit('users', {
-		number: count
-	})
-	socket.broadcast.emit('users', {
-		number: count
-	});
-	socket.on('chat message', function(msg) {
-		io.emit('chat message', msg);
-	});
-	socket.on('disconnect', function() {
-		count--;
-		console.log('User disconnected');
-		socket.broadcast.emit('users', {
-			number: count
-		});
-		socket.emit('users', {
-			number: count
-		});
-	});
-});
+// io.on('connection', function(socket) {
+// 	count++;
+// 	console.log('User connected' + count + 'user(s) present');
+// 	socket.emit('chat message', '你上线了')
+// 	socket.emit('users', {
+// 		number: count
+// 	})
+// 	socket.broadcast.emit('users', {
+// 		number: count
+// 	});
+// 	socket.on('chat message', function(msg) {
+// 		io.emit('chat message', msg);
+// 	});
+// 	socket.on('disconnect', function() {
+// 		count--;
+// 		console.log('User disconnected');
+// 		socket.broadcast.emit('users', {
+// 			number: count
+// 		});
+// 		socket.emit('users', {
+// 			number: count
+// 		});
+// 	});
+// });
